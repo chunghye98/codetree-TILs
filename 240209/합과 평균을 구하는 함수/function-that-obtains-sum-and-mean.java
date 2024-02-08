b5d2e8b8ab39d4ea7dd56fbada36700c5ec48f6f
hyeseon <chunghye1998@gmail.com>
2024-02-09 01:20:29 +0900

    Title: 합과 평균을 구하는 함수, Time: 83ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240209/합과 평균을 구하는 함수/function-that-obtains-sum-and-mean.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int sum = 0;
+        while(st.hasMoreTokens()) {
+            int value = (int) Math.round(Double.parseDouble(st.nextToken()));
+            sum += value;
+        }
+
+        System.out.println(sum);
+        System.out.println(Math.round(sum/3));
+    }
+}
