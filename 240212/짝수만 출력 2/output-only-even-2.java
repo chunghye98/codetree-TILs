a5d75dae40f0dc8cb5060c62a2b4836eddc58b0d
hyeseon <chunghye1998@gmail.com>
2024-02-12 00:00:48 +0900

    Title: 짝수만 출력 2, Time: 171ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240212/짝수만 출력 2/output-only-even-2.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        StringBuilder sb = new StringBuilder();
+        while(a >= b) {
+            if(a % 2 == 0) {
+                sb.append(a+" ");
+            }
+            a--;
+        }
+        System.out.println(sb);
+    }
+}
