7af98b7fa570dc919b160d217092cc4d47b108ae
hyeseon <chunghye1998@gmail.com>
2024-02-11 23:31:21 +0900

    Title: 4번의 크기 비교, Time: 94ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240211/4번의 크기 비교/4-time-comparison.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        while(st.hasMoreTokens()) {
+            if(a > Integer.parseInt(st.nextToken())) {
+                System.out.println("1");
+            }else{
+                System.out.println("0");
+            }
+        }
+    }
+}
