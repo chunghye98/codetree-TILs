0027126200e4e79b44075ff82ca9f21382ac77fe
hyeseon <chunghye1998@gmail.com>
2024-02-12 22:24:24 +0900

    Title: 최소공배수 구하기, Time: 98ms, Memory: 8MB, Status: Passed - CodeTree

--- 240212/최소공배수 구하기/find-the-least-common-multiple.java
+++ 240212/최소공배수 구하기/find-the-least-common-multiple.java
@@ -10,44 +10,16 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        int max = Math.max(n, m);
-        int min = Math.min(n, m);
-        if(max % min == 0) {
-            System.out.println(max);
-            return;
-        }
-
-        List<Integer> nDivisors = findDivisor(n);
-        List<Integer> mDivisors = findDivisor(m);
-
-        List<Integer> divisors = new ArrayList<>();
-        for(Integer value : nDivisors) {
-            if(mDivisors.contains(value)) {
-                divisors.add(value);
-            }
-        }
-
-        if(divisors.size() == 1) {
-            System.out.println(n * m);
-            return;
-        }
-
-        int result = 1;
-        for(Integer d : divisors) {
-            result *= d;
-        }
-
+        int result = (n * m) / gcd(n, m);
         System.out.println(result);
     }
 
-    public static List<Integer> findDivisor(int n){
-        List<Integer> list = new ArrayList<>();
-        for(int i=1; i<n; i++) {
-            if(n % i == 0) {
-                list.add(i);
-            }
+    public static int gcd(int n, int m){
+        if(m == 0) {
+            return n;
         }
-        return list;
+
+        return gcd(m, n%m);
     }
 
 }
