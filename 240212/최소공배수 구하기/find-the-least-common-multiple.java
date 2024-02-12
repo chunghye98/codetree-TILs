47523ebb04cd6eca429d48b69d76f99b08310d15
hyeseon <chunghye1998@gmail.com>
2024-02-12 22:15:55 +0900

    Title: 최소공배수 구하기, Time: 90ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240212/최소공배수 구하기/find-the-least-common-multiple.java
+++ 240212/최소공배수 구하기/find-the-least-common-multiple.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) throws Exception {
         // 여기에 코드를 작성해주세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -9,14 +10,26 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        List<Integer> nDivisor = fidnDivisor(n);
-        List<Integer> mDivisor = fidnDivisor(m);
+        int max = Math.max(n, m);
+        int min = Math.min(n, m);
+        if(max % min == 0) {
+            System.out.println(max);
+            return;
+        }
+
+        List<Integer> nDivisors = findDivisor(n);
+        List<Integer> mDivisors = findDivisor(m);
 
         List<Integer> divisors = new ArrayList<>();
-        for(Integer value : nDivisor) {
-            if(mDivisor.contains(value)) {
+        for(Integer value : nDivisors) {
+            if(mDivisors.contains(value)) {
                 divisors.add(value);
             }
+        }
+
+        if(divisors.size() == 1) {
+            System.out.println(n * m);
+            return;
         }
 
         int result = 1;
@@ -27,13 +40,14 @@ public class Main {
         System.out.println(result);
     }
 
-    public static List<Integer> fidnDivisor(int n) {
+    public static List<Integer> findDivisor(int n){
         List<Integer> list = new ArrayList<>();
-        for(int i=1; i<=n; i++) {
+        for(int i=1; i<n; i++) {
             if(n % i == 0) {
                 list.add(i);
             }
         }
         return list;
     }
+
 }
