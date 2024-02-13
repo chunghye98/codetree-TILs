4ea1ec830327a240e7e5aaf7eb3b7b6eef553900
hyeseon <chunghye1998@gmail.com>
2024-02-13 13:16:42 +0900

    Title: 함수를 이용한 윤년 판별, Time: 85ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240213/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int y = Integer.parseInt(br.readLine());
+
+        boolean result = check(y);
+        System.out.println(result);
+    }
+
+    public static boolean check(int y) {
+        if(y % 4 == 0) {
+            return true;
+        }else if(y%4 == 0 && y % 100 == 0) {
+            return false;
+        }else if(y % 4 == 0 && y % 100 == 0 && y % 400 == 0) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
