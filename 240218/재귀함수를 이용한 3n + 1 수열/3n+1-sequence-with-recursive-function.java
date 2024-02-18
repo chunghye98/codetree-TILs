6c75fe4126c95c754b234b5d689cd1ccb03b45bf
hyeseon <chunghye1998@gmail.com>
2024-02-18 18:55:47 +0900

    Title: 재귀함수를 이용한 3n + 1 수열, Time: 127ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240218/재귀함수를 이용한 3n + 1 수열/3n+1-sequence-with-recursive-function.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int count = 0;
+        System.out.println(solve(n, count));
+    }
+
+    public static int solve(int n, int count) {
+        if(n == 1) return count;
+
+        count++;
+        if(n % 2 == 0) {
+            return solve(n/2, count);
+        }else {
+            return solve(n*3 + 1, count);
+        }
+    }
+}
