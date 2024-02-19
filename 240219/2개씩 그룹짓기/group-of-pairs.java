ffcbfc38f5f00e4342220eed53884da33cac3f3b
hyeseon <chunghye1998@gmail.com>
2024-02-19 16:19:32 +0900

    Title: 2개씩 그룹짓기, Time: 117ms, Memory: 9MB, Status: Passed - CodeTree

--- 240219/2개씩 그룹짓기/group-of-pairs.java
+++ 240219/2개씩 그룹짓기/group-of-pairs.java
@@ -6,23 +6,17 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] arrA = new int[n];
+        int[] arr = new int[n * 2];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=0; i<n; i++) {
-            arrA[i] = Integer.parseInt(st.nextToken());
+        for(int i=0; i<n*2; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        Integer[] arrB = new Integer[n];
-        for(int i=0; i<n; i++){
-            arrB[i] = arrA[i];
-        }
-
-        Arrays.sort(arrA);
-        Arrays.sort(arrB, Collections.reverseOrder());
+        Arrays.sort(arr);
 
         int max = Integer.MIN_VALUE;
         for(int i=0; i<n; i++) {
-            max = Math.max(max, arrA[i] + arrB[i]);
+            max = Math.max(max, arr[i] + arr[n*2 - i - 1]);
         }
 
         System.out.println(max);
