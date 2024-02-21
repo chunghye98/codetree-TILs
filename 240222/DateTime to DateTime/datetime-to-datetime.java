ba532e49022cad6c2b43755bf301787aab52de17
hyeseon <chunghye1998@gmail.com>
2024-02-22 00:09:18 +0900

    Title: DateTime to DateTime, Time: 87ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240222/DateTime to DateTime/datetime-to-datetime.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        if(a <= 11 && b <= 11 && c <= 11) {
+            System.out.println(-1);
+            return;
+        }
+
+        int result = (a * 24 * 60 + b * 60 + c) - (11 * 24 * 60 + 11 * 60 + 11);
+        System.out.println(result);
+    }
+}
