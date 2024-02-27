8c500c69b320e5dec4aca98c18a6cbf627098a91
hyeseon <chunghye1998@gmail.com>
2024-02-27 22:30:22 +0900

    Title: 최대로 겹치는 구간, Time: 97ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240227/최대로 겹치는 구간/maximum-overlapped-segments.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        
+        int[] arr = new int[201];
+        Arrays.fill(arr, 0);
+        StringTokenizer st;
+        for(int i=0; i<n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            for(int j=a+100; j<b+100; j++) {
+                arr[j] += 1;
+            }
+        }
+
+        int max = 0;
+        for(int i=0; i<arr.length; i++) {
+            max = Math.max(max, arr[i]);
+        }
+        System.out.println(max);
+    }
+}
