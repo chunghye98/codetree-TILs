0f2b9768aefa2cb0e55a87214d05668b17d9a2ee
hyeseon <chunghye1998@gmail.com>
2024-03-04 17:19:24 +0900

    Title: 연속되는 수 3, Time: 189ms, Memory: 9MB, Status: Passed - CodeTree

--- 240304/연속되는 수 3/continuous-number3.java
+++ 240304/연속되는 수 3/continuous-number3.java
@@ -15,7 +15,7 @@ public class Main {
         int max = Integer.MIN_VALUE;
         int count = 0;
         for(int i=0; i<n; i++) {
-            if(i >= 1 && arr[i] < 0 && arr[i-1] < 0) {
+            if(i >= 1 && ((arr[i] < 0 && arr[i-1] < 0) || (arr[i] > 0) && arr[i-1] > 0)) {
                 count++;
             }else {
                 count = 1;
