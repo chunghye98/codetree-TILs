57de4e0bdacd7339bfb50b826cf79536aeafaaf7
hyeseon <chunghye1998@gmail.com>
2024-03-05 16:57:47 +0900

    Title: 만나는 그 순간, Time: 88ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240305/만나는 그 순간/the-moment-we-meet.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+
+    static int time = 0;
+    static int location = 0;
+    
+    public static void main(String[] args) throws Exception {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+
+        int[] moveA = new int[100_001];
+        
+        for(int i=0; i<n; i++) {
+            st = new StringTokenizer(br.readLine());
+            String d = st.nextToken();
+            int t = Integer.parseInt(st.nextToken());
+
+            moveA = move(d, t, moveA);
+        }
+
+        time = 0;
+        location = 0;
+        int[] moveB = new int[100_000];
+        for(int i=0; i<m; i++) {
+            st = new StringTokenizer(br.readLine());
+            String d = st.nextToken();
+            int t = Integer.parseInt(st.nextToken());
+
+            moveB = move(d, t, moveB);
+        }
+
+        for(int i=0; i<100_000; i++){
+            if(moveA[i] == moveB[i]) {
+                System.out.println(i);
+                return;
+            }
+        }
+        System.out.println(-1);
+    }
+
+    public static int[] move(String d, int t, int[] arr) {
+        for(int i = time; i < time + t; i++) {
+            arr[i] = location;
+            if(d.equals("L")){
+                location--;
+            }else {
+                location++;
+            }
+        }
+        
+        return arr;
+    }
+}
