c35996a10fbe9f2474acf441fbf58ea669bf828f
hyeseon <chunghye1998@gmail.com>
2024-03-05 22:49:31 +0900

    Title: 문자에 따른 명령 2, Time: 173ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240305/문자에 따른 명령 2/text-based-commands2.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int[] dx = {0, 1, 0, -1}; // 북 동 남 서
+    static int[] dy = {-1, 0, 1, 0};
+
+    public static void main(String[] args) throws Exception{
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputs = br.readLine().split("");
+
+        int dir = 0; // 북
+        int x = 0;
+        int y = 0;
+        for(int i=0; i<inputs.length; i++){
+            if(inputs[i].equals("L")) {
+                dir = (dir - 1 + 4) % 4;
+            }else if(inputs[i].equals("R")) {
+                dir = (dir + 1) % 4;
+            }else {
+                x = dx[dir] + x;
+                y = dy[dir] + y;
+            }
+        }
+        System.out.println(x+" "+y);
+
+    }
+}
