d6bbaa29e29aa831d76a97c97d476fc2621e0517
hyeseon <chunghye1998@gmail.com>
2024-03-05 17:53:55 +0900

    Title: 벌금은 누구에게, Time: 116ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240305/벌금은 누구에게/who-will-pay.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] students = new int[n + 1];
+        Arrays.fill(students, 0);
+        for(int i=0; i<m; i++) {
+            int x = Integer.parseInt(br.readLine());
+            students[x] += 1; 
+        }
+
+        int ans = -1;
+        for(int i=1; i<=n; i++){
+            if(students[i] >= k) {
+                ans = i;
+            }
+        }
+        System.out.println(ans);
+    }
+}
