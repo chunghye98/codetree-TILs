1265687c9a9d84f4bcabd3451522283fa1827910
hyeseon <chunghye1998@gmail.com>
2024-03-06 16:05:09 +0900

    Title: 일렬로 서있는 소 2, Time: 92ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240306/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] cows = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++) {
+            cows[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int count = 0;
+
+        for(int i=0; i<n-2; i++) {
+            for(int j=i+1; j<n-1; j++) {
+                for(int k=j+1; k<n; k++) {
+                    if(cows[i] < cows[j] && cows[j] < cows[k]) {
+                        count++;
+                    }
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
