c7dbab49a57fde74831c73a57ff5fcada7e0899c
hyeseon <chunghye1998@gmail.com>
2024-03-06 17:57:25 +0900

    Title: 구간 중 최대 합, Time: 106ms, Memory: 8MB, Status: Passed - CodeTree

--- 240306/구간 중 최대 합/max-sum-of-subarray.java
+++ 240306/구간 중 최대 합/max-sum-of-subarray.java
@@ -16,7 +16,7 @@ public class Main {
         }
 
         int max = Integer.MIN_VALUE;
-        for(int i=0; i<n-k; i++) {
+        for(int i=0; i<=n-k; i++) {
             int sum = 0;
             for(int j=i; j<i+k; j++) {
                 sum += nums[j];
