e4c068dd7db31ab3ee862f36fabd570ead5ae0bb
hyeseon <chunghye1998@gmail.com>
2024-03-06 18:59:46 +0900

    Title: 특정 구간의 원소 평균값, Time: 127ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240306/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] nums = new int[n];
+        for(int i=0; i <n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int count = 0;
+        for(int i=0; i<n; i++) {
+            int sum = 0;
+            for(int j=i; j<n; j++) {
+                sum += nums[j];
+
+                int avg = sum / (j - i + 1);
+
+                for(int k=i; k<j; k++) {
+                    if(avg == nums[k]) {
+                        count++;
+                        break;
+                    }
+                }
+            }
+            
+        }
+        System.out.println(count);
+
+    }
+}
