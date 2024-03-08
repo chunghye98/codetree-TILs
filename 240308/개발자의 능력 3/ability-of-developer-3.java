e4ba0d743f3990693d02badef03d59973741bcd1
hyeseon <chunghye1998@gmail.com>
2024-03-08 17:16:59 +0900

    Title: 개발자의 능력 3, Time: 90ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240308/개발자의 능력 3/ability-of-developer-3.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int sum = 0;
+    static int[] developers = new int[6];
+
+    public static void main(String[] args) throws Exception{
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for(int i=0; i<6; i++) {
+            developers[i] = Integer.parseInt(st.nextToken());
+            sum += developers[i];
+        }
+
+        int min = Integer.MAX_VALUE;
+        for(int i=0; i<4; i++) {
+            for(int j=1; j<5; j++) {
+                for(int k=2; k<6; k++) {
+                    min = Math.min(min, getTeamSum(i, j, k));
+                }
+            }
+        }
+        System.out.println(min);
+    }
+
+    public static int getTeamSum(int i, int j, int k) {
+        int team1 = developers[i] + developers[j] + developers[k];
+        int team2 = sum - team1;
+        return Math.abs(team1 - team2);
+    }
+}
