e552a7453de6f4159437f418b96790c5ba59767f
hyeseon <chunghye1998@gmail.com>
2024-03-08 17:52:29 +0900

    Title: 개발팀의 능력, Time: 80ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240308/개발팀의 능력/the-capabilities-of-the-development-team.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int[] developers = new int[5];
+    static int sum = 0;
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<5; i++) {
+            developers[i] = Integer.parseInt(st.nextToken());
+            sum += developers[i];
+        }
+
+        int min = Integer.MAX_VALUE;
+
+        for(int i=0; i<5; i++) {
+            for(int j=i+1; j<5; j++) {
+                for(int k=j+1; k<5; k++) {
+                    if(i != j && j != k && k != i) {
+                        min = Math.min(min, getDiff(i, j, k));
+                    }
+                }
+            }
+        }
+        System.out.println(min);
+    }
+
+    public static int getDiff(int i, int j, int k) {
+        int team1 = developers[i] + developers[j];
+        int team2 = developers[k];
+        int team3 = sum - team1 - team2;
+
+        int max = Math.max((Math.max(team1, team2)), team3);
+        int min = Math.min((Math.min(team1, team2)), team3);
+
+        return Math.abs(max - min);
+    }
+}
