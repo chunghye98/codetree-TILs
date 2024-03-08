b4237b3687d857ed3ca671877222d183ff6aa1e9
hyeseon <chunghye1998@gmail.com>
2024-03-08 19:43:33 +0900

    Title: 스승의 은혜 3, Time: 112ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240308/스승의 은혜 3/the-grace-form-teacher-3.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        int[][] students = new int[n][2];
+        for(int i=0; i<n; i++) {
+            st = new StringTokenizer(br.readLine());
+            students[i][0] = Integer.parseInt(st.nextToken());
+            students[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        int max = Integer.MIN_VALUE;
+        for(int i=0; i<n; i++) {
+            int sum = students[i][0] / 2 + students[i][1];
+            int count = 1;
+            for(int j=0; j<n; j++) {
+                if(i == j) continue;
+
+                sum += students[j][0] + students[j][1];
+
+                if(sum > b) {
+                    max = Math.max(max, count);
+                    break;
+                }
+                
+                count++;
+            }
+        }
+        System.out.println(max);
+    }
+}
