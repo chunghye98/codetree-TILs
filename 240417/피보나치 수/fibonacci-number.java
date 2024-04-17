4f1e7619d852f7df1515654644232ed5d3dc597b
hyeseon <chunghye1998@gmail.com>
2024-04-17 18:43:13 +0900

    Title: 피보나치 수, Time: 107ms, Memory: 8MB, Status: Passed - CodeTree

--- 240417/피보나치 수/fibonacci-number.java
+++ 240417/피보나치 수/fibonacci-number.java
@@ -9,8 +9,11 @@ public class Main {
 
         int[] dp = new int[n+1];
         dp[1] = 1;
-        dp[2] = 1;
 
+        if(n > 1) {
+            dp[2] = 1;
+        }
+        
         for(int i=3; i<=n; i++) {
             dp[i] = dp[i-1]+dp[i-2];
         }
