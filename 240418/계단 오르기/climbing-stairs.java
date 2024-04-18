7c37c27987a16a6c2ca81e9a769489640aa01b2b
hyeseon <chunghye1998@gmail.com>
2024-04-18 21:59:36 +0900

    Title: 계단 오르기, Time: 83ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240418/계단 오르기/climbing-stairs.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[n+1];
+
+        dp[1] = 0;
+        dp[2] = 1;
+
+        if(n > 2) {
+            dp[3] = 1;
+        }
+
+        for(int i=4; i<=n; i++) {
+            dp[i] = dp[i-2] + dp[i-3];
+        }
+
+        System.out.println(dp[n]%10007);
+    }
+}
