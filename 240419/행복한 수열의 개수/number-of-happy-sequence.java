5f316d335e814f6165ffb95fbb4fe8ee6521d4a7
hyeseon <chunghye1998@gmail.com>
2024-04-19 18:25:37 +0900

    Title: 행복한 수열의 개수, Time: 96ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240419/행복한 수열의 개수/number-of-happy-sequence.java
+++ 240419/행복한 수열의 개수/number-of-happy-sequence.java
@@ -18,13 +18,42 @@ public class Main {
 
         int result = 0;
 
+        if(m == 1) {
+            // 행 검사
+            for(int i=0; i<n; i++) {
+                int num = map[i][0];
+                int count = 1;
+                for(int j=1; j<n; j++) {
+                    if(map[i][j] != num) {
+                        result++;
+                        break;
+                    }
+                }
+            }
+
+            for(int j=0; j<n; j++) {
+                int num = map[0][j];
+                int count = 1;
+                for(int i=1; i<n; i++) {
+                    if(map[i][j] != num) {
+                        result++;
+                        break;
+                    }
+                }
+            }
+            System.out.println(result);
+            return;
+        }
+
         // 행 검사 
         for(int i=0; i<n; i++) {
             int num = map[i][0];
             int count = 1;
             for(int j=1; j<n; j++) {
+                System.out.println("map[i][j]: "+map[i][j]);
                 if(map[i][j] == num) {
                     count++;
+                    System.out.println("row count: "+count);
                     if(count >= m) { // 같은 값이 m개가 되면 더 셀 필요 없으므로 break 
                         result++;
                         break;
@@ -43,6 +72,7 @@ public class Main {
             for(int i=1; i<n; i++) {
                 if(map[i][j] == num) {
                     count++;
+                    System.out.println("col count: "+count);
                     if(count >= m) {
                         result++;
                         break;
@@ -54,6 +84,6 @@ public class Main {
             }
         }
         System.out.println(result);
-
+            
     }
 }
