eb00f5261558a82608088078dbe7bd490f1cceda
hyeseon <chunghye1998@gmail.com>
2024-04-22 23:00:27 +0900

    Title: 트로미노, Time: 194ms, Memory: 12MB, Status: Passed - CodeTree

--- 240422/트로미노/tromino.java
+++ 240422/트로미노/tromino.java
@@ -2,11 +2,15 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
+    static int n;
+    static int m;
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
         int[][] map = new int[n][m];
         for(int i=0; i<n; i++) {
@@ -17,8 +21,10 @@ public class Main {
         }
 
         int max1 = Integer.MIN_VALUE;
-        for(int i=0; i<n-1; i++) {
-            for(int j=0; j<m-1; j++) {
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                if(!checkSquare(i, j)) continue;
+
                 // 반대 ㄴ 자 블럭
                 max1 = Math.max(max1, map[i][j+1] + map[i+1][j] + map[i+1][j+1]);
 
@@ -34,16 +40,36 @@ public class Main {
         }
 
         int max2 = Integer.MIN_VALUE;
-        for(int i=0; i<n-2; i++) {
-            for(int j=0; j<m-2; j++) {
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
                 // ㅡ자 블럭
-                max2 = Math.max(max2, map[i][j] + map[i][j+1] + map[i][j+2]);
+                if(lineCheck(j, m)) {
+                    max2 = Math.max(max2, map[i][j] + map[i][j+1] + map[i][j+2]);
+                } 
 
                 // |자 블럭
-                max2 = Math.max(max2, map[i][j] + map[i+1][j] + map[i+2][j]);
+                if(lineCheck(i, n)) {
+                    max2 = Math.max(max2, map[i][j] + map[i+1][j] + map[i+2][j]);
+                }
             }
         }
         
         System.out.println(Math.max(max1, max2));
+    }
+
+    public static boolean lineCheck(int x, int l) {
+        if(x < 0 || x+1 < 0 || x+2 < 0 || x >= l || x+1 >= l || x+2 >= l) {
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean checkSquare(int i, int j) {
+        if(i < 0 || i >= n || j < 0 || j >= m) {
+            return false;
+        }else if(i+1 < 0 || i+1 >= n || j+1 < 0 || j+1 >= m) {
+            return false;
+        }
+        return true;
     }
 }
