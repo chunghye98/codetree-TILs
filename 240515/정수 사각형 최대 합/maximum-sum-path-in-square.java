8716dd8db555b9590f386f4119ec9ec524c28672
hyeseon <chunghye1998@gmail.com>
2024-05-15 17:00:31 +0900

    Title: 정수 사각형 최대 합, Time: 169ms, Memory: 12MB, Status: Passed - CodeTree

--- 240515/정수 사각형 최대 합/maximum-sum-path-in-square.java
+++ 240515/정수 사각형 최대 합/maximum-sum-path-in-square.java
@@ -2,37 +2,48 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
+    static int n;
+    static int[][] map;
+    static int[][] dp;
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(br.readLine());
 
-        int[][] map = new int[101][101];
+        map = new int[n][n];
+        dp = new int[n][n];
+
         StringTokenizer st;
-        for(int i=1; i<=n; i++) {
+        for(int i=0; i<n; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j=1; j<=n; j++) {
+            for(int j=0; j<n; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        int[][] dp = new int[101][101];
-        dp[1][1] = map[1][1];
-        for(int i=1; i<=n; i++) {
-            for(int j=1; j<=n; j++) {
-                // 오른쪽 이동
-                int now = dp[i][j];
-                int right = map[i][j+1];
-                if(dp[i][j+1] < now + right) {
-                    dp[i][j+1] = now + right;
-                }
-                
-                // 밑으로 이동
-                int down = map[i+1][j];
-                if(dp[i+1][j] < now + down) {
-                    dp[i+1][j] = now + down;
-                }
+        // 초기값 설정
+        initialize();
+        
+        for(int i=1; i<n; i++) {
+            for(int j=1; j<n; j++) {
+                dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1])+map[i][j];
             }
         }
-        System.out.println(dp[n][n]);
+        System.out.println(dp[n-1][n-1]);
+    }
+
+    public static void initialize() {
+        dp[0][0] = map[0][0];
+
+        // 최좌측 초기값
+        for(int i=1; i<n; i++) {
+            dp[i][0] = dp[i-1][0] + map[i][0];
+        }
+
+        // 최상단 초기값
+        for(int j=1; j<n; j++) {
+            dp[0][j] = dp[0][j-1] + map[0][j];
+        }
     }
 }
