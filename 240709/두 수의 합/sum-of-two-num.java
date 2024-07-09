5b36034a72a3ca6ca9e0a4b1e39c529e7901440b
hyeseon <chunghye1998@gmail.com>
2024-07-09 16:22:02 +0900

    Title: 두 수의 합, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240709/두 수의 합/sum-of-two-num.java
+++ 240709/두 수의 합/sum-of-two-num.java
@@ -13,25 +13,29 @@ public class Main {
         inputs = br.readLine().split(" ");
         for(int i=0; i<n; i++) {
             int key = Integer.parseInt(inputs[i]);
-            if(map.get(key) != null && key*2 == k) {
+            if(map.get(key) != null) {
                 map.put(key, map.get(key)+1);
                 continue;
             }
-            map.put(key, 0);
-        }
-
-        // 2. map 돌면서 (k-key)가 null이 아니면 value+1
-        for(int key : map.keySet()) {
-            if(map.get(k-key) != null) {
-                map.put(key, map.get(key)+1);
-            }
+            map.put(key, 1);
         }
         
-        // 3. map 돌면서 value 누적해서 출력
+        // 2. map 돌면서 value 누적해서 출력
         int sum = 0;
         for(int key : map.keySet()) {
-            sum += map.get(key);
+            if(map.get(k-key) == 1) {
+                sum += map.get(key);
+            }
         }
-        System.out.println(sum/2);
+        sum /= 2;
+
+        for(int key : map.keySet()) {
+            if(map.get(key) > 1) {
+                for(int i=map.get(key)-1 ; i>=0; i--) {
+                    sum += i;
+                }
+            }
+        }
+        System.out.println(sum);
     }
 }
