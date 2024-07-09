e037eb5cafc35ff16ba5af7faf99a09864c9c653
hyeseon <chunghye1998@gmail.com>
2024-07-09 16:39:55 +0900

    Title: 두 수의 합, Time: 415ms, Memory: 27MB, Status: Passed - CodeTree

--- 240709/두 수의 합/sum-of-two-num.java
+++ 240709/두 수의 합/sum-of-two-num.java
@@ -9,33 +9,29 @@ public class Main {
         int n = Integer.parseInt(inputs[0]);
         int k = Integer.parseInt(inputs[1]);
 
-        Map<Integer, Integer> map = new HashMap<>();
         inputs = br.readLine().split(" ");
+        int[] input = new int[n];
         for(int i=0; i<n; i++) {
-            int key = Integer.parseInt(inputs[i]);
+            input[i] = Integer.parseInt(inputs[i]);
+        }
+
+        // 2. 입력으로 주어진 수의 갯수를 value로 넣는다.
+        // 3. 합쳐서 k가 되는 수가 있다면 해당 수의 value를 result에 누적한다.
+        Map<Integer, Integer> map = new HashMap<>();
+        int result = 0;
+        for(int i=0; i<n; i++) {
+            int key = input[i];
+
+            if(map.containsKey(k-key)){
+                result += map.get(k-key);
+            }
+
             if(map.get(key) != null) {
                 map.put(key, map.get(key)+1);
                 continue;
             }
             map.put(key, 1);
         }
-        
-        // 2. map 돌면서 value 누적해서 출력
-        int sum = 0;
-        for(int key : map.keySet()) {
-            if(map.get(k-key) != null && map.get(k-key) == 1) {
-                sum += map.get(key);
-            }
-        }
-        sum /= 2;
-
-        for(int key : map.keySet()) {
-            if(map.get(key) > 1) {
-                for(int i=map.get(key)-1 ; i>=0; i--) {
-                    sum += i;
-                }
-            }
-        }
-        System.out.println(sum);
+        System.out.println(result);
     }
 }
