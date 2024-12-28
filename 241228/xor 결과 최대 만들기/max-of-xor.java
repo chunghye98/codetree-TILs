c10055a63a016dad8b744cd268f1e69b44efa38b
hyeseon <chunghye1998@gmail.com>
2024-12-28 22:59:52 +0900

    Title: xor 결과 최대 만들기, Time: 82ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 241228/xor 결과 최대 만들기/max-of-xor.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int max;
+    static int N, M;
+    static int[] numbers;
+    static List<Integer> results;
+
+    public static void main(String[] args) throws Exception {
+        init();
+        solve(1,0);
+        output();
+    }
+
+    public static void init() throws Exception{
+        String[] inputs = br.readLine().split(" ");
+        N = Integer.parseInt(inputs[0]);
+        M = Integer.parseInt(inputs[1]);
+
+        inputs = br.readLine().split(" ");
+        numbers = new int[N];
+        for(int i=0; i<N; i++) {
+            numbers[i] = Integer.parseInt(inputs[i]);
+        }
+
+        max = Integer.MIN_VALUE;
+        results = new ArrayList<>();
+    }
+
+    public static void solve(int curN, int cnt) {
+        if(cnt == M) {
+            int value = results.get(0);
+            for(int i=1; i<results.size(); i++) {
+                value = value ^ results.get(i);
+            }
+            max = Math.max(value, max);
+            return;
+        }
+
+        for(int i=curN; i<=N; i++) {
+            results.add(i);
+            solve(i+1, cnt+1);
+            results.remove(results.size()-1);
+        }
+    }
+
+    public static void output() {
+        System.out.println(max);
+    }
+}
